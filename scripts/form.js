e7b7a8f85d94c873670171633793f2910375ad7f
Rony Reyes <rony@caidci.com>
2025-11-28 12:31:27 -0800

    adding the form.html

--- scripts/form.js
+++ scripts/form.js
@@ -1,16 +1,35 @@
-const products = [
-  { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-  { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-  { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-  { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-  { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
-];
-
 const select = document.getElementById("product");
 
-products.forEach(product => {
-    const option = document.createElement("option");
-    option.value = product.id;
-    option.textContent = product.name;
-    select.appendChild(option);
+if (select) {
+    const products = [
+        { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+        { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+        { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+        { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+        { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+    ];
+
+    products.forEach(product => {
+        const option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name;
+        select.appendChild(option);
+    });
+}
+
+const yearSpan = document.getElementById("year");
+const lastModifiedSpan = document.getElementById("lastModified");
+
+if (yearSpan) yearSpan.textContent = new Date().getFullYear();
+if (lastModifiedSpan) lastModifiedSpan.textContent = document.lastModified;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const countElement = document.getElementById("count");
+
+    if (countElement) {
+        let count = Number(localStorage.getItem("reviewCount")) || 0;
+        count++;
+        localStorage.setItem("reviewCount", count);
+        countElement.textContent = count;
+    }
 });
